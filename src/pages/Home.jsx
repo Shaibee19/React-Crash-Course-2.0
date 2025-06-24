cb51a69fa8f0c1ddd2877fd9b304fef89d879221
Shaibee19 <shaipattzgray@gmail.com>
2025-06-24 14:58:47 -0400

    working on adding API to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import User from "../components/User";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [users, setUsers] = useState([]);
-  async function main() {
+  async function fetchUsers() {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     setUsers(data);
+    console.log(data);
   }
 
   useEffect(() => {
-    main();
+    setTimeout(() => {
+      fetchUsers();
+    }, 1000);
   }, []);
-  
-  return <h1>{users[0].name}</h1>;
+
+//   const pixels = "3px";
+
+  return (
+    <div>
+      {users.map((user) => (
+        <Link to={`/users/${user.id}`} key={user.id}>
+        <User
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
